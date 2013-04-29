95afe5bd53271f7823db0abd7eddb91f75c7a2cc
Chris Gilley <chris@gilleydigital.com>
2013-04-29 17:23:30 -0400

    Fixed fatal bug & abstracted folder name

--- classes/Controller/Download.php
+++ classes/Controller/Download.php
@@ -1,18 +1,20 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 class Controller_Download extends Controller {
+	private $folder = 'downloads';
 	
 	// Public download links
 	public function action_public()
 	{
 		// Look for the file in the downloads folder in document root
 		$file_name = $this->request->param('file');
-		$file_location = DOCROOT.'/downloads/'.$file_name;
+		$folder = $this->folder;
+		$file_location = DOCROOT.DIRECTORY_SEPARATOR.$folder.DIRECTORY_SEPARATOR.$file_name;
 		
 		// Send file if it exists, 404 if it doesn't
-		if(file_exists($file_location))
+		if (file_exists($file_location))
 		{
-			Request::current()->response()->send_file($file_location);
+			$this->response->send_file($file_location);
 		}
 		else{
 			throw new HTTP_Exception_404($file_name.' not found');
